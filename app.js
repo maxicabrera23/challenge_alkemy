c2ac178eecb5a82b2630b1ebb3137ba88a6360ac
unknown <maxicabrera23@gmail.com>
2021-08-26 01:41:29 -0300

    first commit basic web construction

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const app = express();
+const port = 3000;
+
+
+app.use (express.static(__dirname + '/public'));
+app.get('/', (req, res)=> {
+    res.sendFile('/public/index.html')
+
+});
+ 
+
+app.listen(port,()=>{
+console.log( `server runing in port:${port}...` )
+
+});
